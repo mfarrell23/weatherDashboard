586a8f01b153e6ecddc2e706ad66ed25e7c4014c
michelle <113373744+mfarrell23@users.noreply.github.com>
2022-10-10 13:54:40 -0600

    updates to html and java

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+var searchBtn= document.querySelector("#button")
+
+searchBtn.addEventListener("click",function(){
+    console.log(abc.value)
+    localStorage.setItem("",JSON.stringify(abc.value);
